cfda29447ded1de144f2e451068c35b294b562fc
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-24 02:28:58 -0300

    Evaluación sobre giro
    Detecta para qué lado se movió.
    Bloqueo de giro de pantalla.

--- movimiento/www/js/controllers.js
+++ movimiento/www/js/controllers.js
@@ -1,5 +1,130 @@
 angular.module('starter.controllers', [])
 
+.controller('DashCtrl', function($scope, $cordovaDeviceMotion, $ionicPlatform) {
+    
+    $scope.resultado;
+
+    $scope.coordenadas = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
+    };
+
+    $scope.coordenadasAnteriores = {
+        x : null,
+        y : null,
+        z : null,
+        timestamp : null
+    };
+
+    $scope.opciones = {frequency: 1000, deviation: 25};
+
+
+  //Start Watching method
+  $scope.Mirar = function() {
+   
+      // Device motion configuration
+      $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.opciones);
+   
+      // Device motion initilaization
+      $scope.watch.then(null, function(error) {
+          console.log('Error');
+      },function(result) {
+   
+          // Set current data  
+          $scope.coordenadas.x = Math.round(result.x*100);
+          $scope.coordenadas.y = Math.round(result.y*100);
+          $scope.coordenadas.z = Math.round(result.z*100);
+          $scope.coordenadas.timestamp = result.timestamp;
+   
+          // Detecta shake  
+          $scope.Detectar(result);
+          //Evalúo quién se movió
+          $scope.Evaluar();
+       });     
+  };  
+
+
+  //Stop watching method
+  $scope.Parar = function() {  
+      $scope.watch.clearWatch();
+  }
+
+
+
+  // Detect shake method      
+  $scope.Detectar = function(result) { 
+   
+      //Object to hold measurement difference between current and old data
+      var coordenadasDiferencia = {};
+   
+      // Calculate measurement change only if we have two sets of data, current and old
+      if ($scope.coordenadasAnteriores.x !== null) {
+          coordenadasDiferencia.x = Math.abs($scope.coordenadasAnteriores.x, result.x);
+          coordenadasDiferencia.y = Math.abs($scope.coordenadasAnteriores.y, result.y);
+          coordenadasDiferencia.z = Math.abs($scope.coordenadasAnteriores.z, result.z);
+      }
+   
+      // If measurement change is bigger then predefined deviation
+      if (coordenadasDiferencia.x + coordenadasDiferencia.y + coordenadasDiferencia.z > $scope.opciones.deviation) {
+          $scope.Parar();  // Stop watching because it will start triggering like hell
+          console.log('Shake detected'); // shake detected
+          setTimeout($scope.Mirar(), 1000);  // Again start watching after 1 sex
+   
+          // Clean previous measurements after succesfull shake detection, so we can do it next time
+          $scope.coordenadasAnteriores = { 
+              x: null, 
+              y: null, 
+              z: null
+          }
+
+          
+          $scope.Evaluar();
+      } else {
+          // On first measurements set it as the previous one
+          $scope.coordenadasAnteriores = {
+              x: result.x,
+              y: result.y,
+              z: result.z
+          }
+      }           
+  }   
+
+  $scope.Evaluar = function(){
+      if(Math.abs($scope.coordenadas.x)>Math.abs($scope.coordenadas.y)){
+          //Fue más relevante el movimiento al costado
+          if (Math.abs($scope.coordenadas.x)>700){
+              //Fue relevante
+              if($scope.coordenadas.x > 0){
+                  //Giré a la izquierda
+                  $scope.coordenadas.resultado = "Izquierda";
+              }
+              else{
+                  //Giré a la derecha
+                  $scope.coordenadas.resultado = "Derecha";
+              }
+          }
+      }
+      else{
+          //Fue más relevante el movimiento vertical
+          if (Math.abs($scope.coordenadas.y)>700){
+              //Fue relevante
+              if($scope.coordenadas.y > 0){
+                  //Giré hacia abajo
+                  $scope.coordenadas.resultado = "Abajo";
+              }
+              else{
+                  //Giré hacia arriba
+                  $scope.coordenadas.resultado = "Arriba";
+              }
+          }
+      }
+  };
+
+
+})
+
 .controller('ChatsCtrl', function($scope, $cordovaDeviceMotion, $ionicPlatform) {
     
     $scope.coordenadas = {
@@ -84,14 +209,6 @@ angular.module('starter.controllers', [])
           }
       }           
   }   
-
-})
-
-.controller('DashCtrl', function($scope, $cordovaDeviceMotion, $ionicPlatform) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
